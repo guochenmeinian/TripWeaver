01685a0658088176bf7dc4fcb40486bcd57935fb
guochenmeinian <guochenmeinian@outlook.com>
2025-05-30 00:49:26 -0400

    very first prototype

---
+++ TripWeaver/agent.py
@@ -0,0 +1,23 @@
+# agent.py
+"""Demonstration of Travel AI Conceirge using Agent Development Kit"""
+
+from google.adk.agents import Agent
+from TripWeaver.prompt import ROOT_AGENT_INSTR
+
+from TripWeaver.tools.memory import _load_precreated_itinerary
+
+root_agent = Agent(
+    model="gemini-2.0-flash-001",
+    name="root_agent",
+    description="A Travel Conceirge using the services of multiple sub-agents",
+    instruction=ROOT_AGENT_INSTR,
+    # sub_agents=[
+    #     inspiration_agent,
+    #     planning_agent,
+    #     booking_agent,
+    #     pre_trip_agent,
+    #     in_trip_agent,
+    #     post_trip_agent,
+    # ],
+    before_agent_callback=_load_precreated_itinerary,
+)
--- adk/agent.py
+++
@@ -1,4 +0,0 @@
-# adk/agent.py
-class Agent:
-    def run(self, input_data: dict, **kwargs) -> dict:
-        raise NotImplementedError("Each agent must implement the run method.")
