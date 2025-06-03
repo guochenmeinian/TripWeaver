37747944616708bc03868d8ddc50a83a2a6fcd2c
guochenmeinian <guochenmeinian@outlook.com>
2025-06-02 20:17:23 -0400

    pre_trip draft done

--- TripWeaver/sub_agents/pre_trip/prompt.py
+++ TripWeaver/sub_agents/pre_trip/prompt.py
@@ -10,7 +10,7 @@ The current profile state is shown below:
 </user_profile>
 
 Your task:
-- Identify which of the following fields are missing or empty:
+- Fill the user profile with the following fields:
   - passport_nationality
   - seat_preference
   - food_preference
@@ -25,21 +25,22 @@ Your task:
 Return the response as a JSON object:
 {{
   "user_profile": {{
-    "passport_nationality": "US Citizen",
-    "seat_preference": "window",
-    "food_preference": "vegetarian",
-    "allergies": ["peanuts"],
-    "likes": ["museums", "mountains"],
-    "dislikes": ["crowds"],
-    "price_sensitivity": ["medium"],
+    "passport_nationality": "",
+    "seat_preference": "",
+    "food_preference": "",
+    "allergies": [],
+    "likes": [],
+    "dislikes": [],
+    "price_sensitivity": [""],
     "home": {{
       "event_type": "home",
-      "address": "123 Main St, NY, USA",
-      "local_prefer_mode": "drive"
+      "address": "",
+      "local_prefer_mode": ""
     }}
   }}
 }}
 
+DO NOT MAKE UP THE INFORMATION, JUST RETURN WHAT THE USER SAID.
 """
 
 PRETRIP_COLLECTOR_INSTR = """
@@ -47,7 +48,8 @@ You are a top-level assistant in charge of making sure the user's travel profile
 
 Your job:
 1. Use the `ask_profile_agent` tool to update the user profile fields.
-2. If the `ask_profile_agent` tool returns a complete profile, transfer the user to inspiration_agent. Otherwise, continue to ask questions.
+2. If the `ask_profile_agent` tool returns a incomplete profile (all fields must not be empty), continue to ask questions.
+3. If the `ask_profile_agent` tool returns a complete profile, transfer the user to inspiration_agent.
 
 Required fields to check:
 - passport_nationality
