7aa1eee61c7207666d6a9eada9785596b1bb8f61
Xiao-1231751 <104030203+Xiao-1231751@users.noreply.github.com>
2025-06-03 18:19:05 -0400

    airbnb agent works

--- TripWeaver/agent.py
+++ TripWeaver/agent.py
@@ -10,16 +10,18 @@ from TripWeaver.sub_agents.pre_trip.agent import pre_trip_agent
 from TripWeaver.sub_agents.housing.agent import airbnb_agent
 from TripWeaver.tools.memory import _load_precreated_itinerary
 
+sub_agents = [
+    inspiration_agent,
+    pre_trip_agent,
+    airbnb_agent, 
+]
+
+
 root_agent = LlmAgent(
     model="gemini-2.0-flash-001",
     name="root_agent",
     description="A Travel Conceirge using the services of multiple sub-agents",
     instruction=ROOT_AGENT_INSTR,
-    sub_agents=[
-        inspiration_agent,
-    #     planning_agent,
-        pre_trip_agent,
-        airbnb_agent,
-    ],
+    sub_agents=sub_agents,
     before_agent_callback=_load_precreated_itinerary,
 )
